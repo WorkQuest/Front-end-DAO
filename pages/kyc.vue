0c5f170e5f6d1fe1ae76ebdca8e102227fe89079
JuSTeRFeeD <justerfeedy@gmail.com>
2021-11-29 17:28:35 +0700

    feature/kyc base kyc logic from wq

--- pages/kyc.vue
+++ pages/kyc.vue
@@ -4,14 +4,77 @@
       <div class="kyc__header">
         {{ $t('kyc.KYC') }}
       </div>
-      <div class="kyc__content" />
+      <div class="kyc__content">
+        <div id="sumsub-websdk-container" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import snsWebSdk from '@sumsub/websdk';
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'KYC',
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters({
+      accessToken: 'sumsub/getSumSubBackendToken',
+      userData: 'user/getUserData',
+    }),
+  },
+  async mounted() {
+    this.SetLoader(true);
+    await this.createAccessToken();
+    // await this.getApiHealthStatus();
+    this.initSumSub();
+    this.SetLoader(false);
+  },
+  methods: {
+    async createAccessToken() {
+      try {
+        const payload = {
+          userId: this.accessToken.userId,
+        };
+        await this.$store.dispatch('sumsub/createAccessTokenBackend', payload);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async getApiHealthStatus() {
+      try {
+        const response = await this.$store.dispatch('sumsub/apiHealth');
+        console.log(response.result);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    initSumSub() {
+      const accessToken = this.accessToken.token;
+      const applicantEmail = this.userData.email;
+      const applicantPhone = this.userData.phone;
+
+      const snsWebSdkInstance = snsWebSdk.Builder('https://test-api.sumsub.com', 'basic-kyc')
+        .withAccessToken(accessToken, () => {
+        })
+        .withConf({
+          lang: 'en',
+          email: applicantEmail,
+          phone: applicantPhone, // if available
+          onMessage: (type, payload) => {
+            console.log('WebSDK onMessage', type, payload);
+          },
+          onError: (error) => {
+            console.log('WebSDK onError', error);
+          },
+        }).build();
+
+      snsWebSdkInstance.launch('#sumsub-websdk-container');
+    },
+  },
 };
 </script>
 
