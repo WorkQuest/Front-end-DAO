58a645ef238a8fe24a1dcfb098ea90c5ae11565d
ghost1 <sonofagronom@gmail.com>
2021-12-29 13:28:56 +0700

    hotfix/18824/dao-hotfix added files clear in uploader after submit

--- pages/discussions/_id/index.vue
+++ pages/discussions/_id/index.vue
@@ -334,7 +334,6 @@ export default {
       await this.$store.dispatch('discussions/sendCommentOnDiscussion', { id: this.currentDiscussion.id, payload });
       this.isAddComment = false;
       this.opinion = '';
-      this.documents = '';
       await this.getRootComments();
     },
     async getCurrentDiscussion() {
