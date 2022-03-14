6e48b11ad44c9a064065fb31e48c403e92d44c7e
ghost1 <sonofagronom@gmail.com>
2022-03-14 15:27:30 +0700

    fix/24245/fix errors, added screen 'Your profile already verified!'

--- pages/kyc.vue
+++ pages/kyc.vue
@@ -5,7 +5,20 @@
         {{ $t('kyc.KYC') }}
       </div>
       <div class="kyc__content">
-        <div id="sumsub-websdk-container" />
+        <div
+          v-if="statusKYC === 0"
+          id="sumsub-websdk-container"
+        />
+        <div class="kyc__verified">
+          <img
+            class="kyc__image"
+            src="~/assets/img/ui/questAgreed.svg"
+            alt=""
+          >
+          <div class="kyc__text">
+            {{ $t('kyc.alreadyVerified') }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -17,13 +30,11 @@ import { mapGetters } from 'vuex';
 
 export default {
   name: 'KYC',
-  data() {
-    return {};
-  },
   computed: {
     ...mapGetters({
       accessToken: 'sumsub/getSumSubBackendToken',
       userData: 'user/getUserData',
+      statusKYC: 'user/getStatusKYC',
     }),
   },
   async mounted() {
@@ -34,29 +45,31 @@ export default {
   },
   methods: {
     async createAccessToken() {
-      await this.$store.dispatch('sumsub/createAccessTokenBackend', { userId: this.accessToken.userId });
+      if (this.statusKYC === 0) await this.$store.dispatch('sumsub/createAccessTokenBackend', { userId: this.accessToken.userId });
     },
     initSumSub() {
-      const accessToken = this.accessToken.token;
-      const applicantEmail = this.userData.email;
-      const applicantPhone = this.userData.phone;
+      if (this.statusKYC === 0) {
+        const accessToken = this.accessToken.token;
+        const applicantEmail = this.userData.email;
+        const applicantPhone = this.userData.phone;
 
-      try {
-        const snsWebSdkInstance = snsWebSdk.Builder('https://test-api.sumsub.com', 'basic-kyc')
-          .withAccessToken(accessToken, () => {
-          })
-          .withConf({
-            lang: 'en',
-            email: applicantEmail,
-            phone: applicantPhone,
-            onError: (error) => {
-              console.log('WebSDK onError', error);
-            },
-          }).build();
+        try {
+          const snsWebSdkInstance = snsWebSdk.Builder('https://test-api.sumsub.com', 'basic-kyc')
+            .withAccessToken(accessToken, () => {
+            })
+            .withConf({
+              lang: 'en',
+              email: applicantEmail,
+              phone: applicantPhone,
+              onError: (error) => {
+                console.log('WebSDK onError', error);
+              },
+            }).build();
 
-        snsWebSdkInstance.launch('#sumsub-websdk-container');
-      } catch (e) {
-        console.error('Error with snsWebSdkInstance. ', e);
+          snsWebSdkInstance.launch('#sumsub-websdk-container');
+        } catch (e) {
+          console.error('Error with snsWebSdkInstance. ', e);
+        }
       }
     },
   },
@@ -67,10 +80,12 @@ export default {
 .kyc {
   @include main;
   @include text-simple;
+
   &__body {
     max-width: 1180px;
     height: 100%;
   }
+
   &__header {
     font-weight: 600;
     font-size: 28px;
@@ -78,12 +93,33 @@ export default {
     color: #000000;
     margin: 30px 0 20px;
   }
+
+  &__image {
+    height: 100px;
+    width: 100px;
+  }
+
+  &__text {
+    margin-top: 10px;
+  }
+
+  &__verified {
+    padding: 20px 0 20px 0;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
   &__content {
+    display: flex;
     height: calc(100% - 86px);
     background: #FFFFFF;
     border-radius: 6px;
+    justify-content: center;
+    align-items: center;
   }
 }
+
 @include _767 {
   .kyc {
     &__header {
