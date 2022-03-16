196434825baed0181cfbd5edb03b52516b3ca743
ghost1 <sonofagronom@gmail.com>
2022-03-16 12:26:05 +0700

    fix/24253/fix-phone-in-investor-profile fixed second mobile number

--- pages/investors/_id/index.vue
+++ pages/investors/_id/index.vue
@@ -297,7 +297,12 @@ export default {
         if (input.key === 'location') return this.investor.locationPlaceName;
         if (input.key === 'phone' && this.investor.phone) return this.investor.phone.fullPhone;
         if (input.key === 'phone' && this.investor.tempPhone) return this.investor.tempPhone.fullPhone;
-        if (input.key === 'secondPhone' && this.investor.secondMobileNumber) return this.investor.secondMobileNumber.fullPhone;
+        if (input.key === 'secondPhone') {
+          if (this.userData.id === this.userId && this.investor.additionalInfo.secondMobileNumber) {
+            return this.investor.additionalInfo.secondMobileNumber.fullPhone;
+          }
+          if (this.investor.secondMobileNumber) return this.investor.secondMobileNumber.fullPhone;
+        }
         return this.investor[input.key];
       }
       return '';
