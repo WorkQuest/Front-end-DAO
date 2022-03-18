8535f08a0c356bf585ebbf9e0903fd49e26a6067
AlexanderBess <78302587+AlexanderBess@users.noreply.github.com>
2022-03-18 19:28:07 +0700

    feature/24568/validators fix validator page

--- pages/validators/_id/index.vue
+++ pages/validators/_id/index.vue
@@ -1,33 +1,98 @@
 <template>
   <div class="validator">
     <div class="validator__body">
-      <div class="validator__back">
-        <nuxt-link
-          class="back__link"
-          to="/validators"
-        >
-          <span class="back__icon icon-short_left" />
-          {{ $t('proposal.back') }}
-        </nuxt-link>
-      </div>
-      <div class="validator__title">
-        {{ $t('validator.title') }}
-      </div>
-      <div class="validator__profile profile">
-        <div class="profile__top">
-          <img
-            class="profile__avatar"
-            :src="require('~/assets/img/app/avatar_empty.png')"
-            :alt="'empty avatar'"
+      <div class="validator__head">
+        <div class="validator__back back">
+          <nuxt-link
+            class="back__link"
+            to="/validators"
           >
-          <div class="top__info">
-            <div class="top__user">
-              <div class="top__name">
-                Name Namew
+            <span class="back__icon icon-short_left" />
+            {{ $t('proposal.back') }}
+          </nuxt-link>
+        </div>
+        <div class="validator__title">
+          {{ $t('validator.title') }}
+        </div>
+      </div>
+      <div class="validator__block">
+        <div class="validator__profile profile">
+          <div class="profile__left">
+            <img
+              class="profile__avatar"
+              :src="require('~/assets/img/app/avatar_empty.png')"
+              :alt="'empty avatar'"
+            >
+            <div class="left__info">
+              <div class="left__user">
+                <div class="left__text left__text_name">
+                  Name SecondName
+                </div>
+                <div class="left__date">
+                  Created on 22 Aug, 2021
+                </div>
               </div>
-              <div class="top__some-info">
-                123
+            </div>
+          </div>
+          <div class="profile__left profile__left_data">
+            <div
+              v-for="(item, key) in leftColumn"
+              :key="key"
+              class="left__info"
+            >
+              <div class="left__user">
+                <div class="left__text">
+                  {{ item.name }}
+                </div>
+                <div class="left__date">
+                  {{ item.desc }}
+                </div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div class="validator__profile profile">
+          <div class="profile__right">
+            <div class="right__bar">
+              <div class="bar__info">
+                <div class="bar__text">
+                  {{ $t('validator.slots') }}
+                </div>
+                <div class="bar__data">
+                  <div class="bar__text bar__text_grey">
+                    {{ `${$t('validator.occupied')}:` }}
+                  </div>
+                  <div class="bar__text bar__text_small">
+                    {{ `395 ${$t('validator.outOf')} 1000` }}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="right__block">
+              <div class="right__info">
+                <div class="right__data-name">
+                  {{ $t('validator.delegators') }}
+                </div>
+                <div class="right__data-desc">
+                  337
+                </div>
+              </div>
+              <div class="right__info">
+                <div class="right__data-name">
+                  {{ $t('validator.minimalStake') }}
+                </div>
+                <div class="right__data-desc">
+                  {{ $tc('validator.wusdCount', 0) }}
+                </div>
+              </div>
+            </div>
+            <div class="right__block">
+              <button class="right__button right__button_red">
+                {{ $t('validator.undelegate') }}
+              </button>
+              <button class="right__button right__button_blue">
+                {{ $t('validator.delegate') }}
+              </button>
             </div>
           </div>
         </div>
@@ -39,6 +104,16 @@
 <script>
 export default {
   name: 'Index',
+  computed: {
+    leftColumn() {
+      return [
+        { name: this.$t('validator.address'), desc: '0xnf…thb3' },
+        { name: this.$t('validator.commonStake'), desc: this.$tc('validator.wusdCount', 1000000) },
+        { name: this.$t('validator.fee'), desc: '5%' },
+        { name: this.$t('validator.missedBlocks'), desc: 1000 },
+      ];
+    },
+  },
 };
 </script>
 
@@ -49,16 +124,22 @@ export default {
   &__body {
     margin-top: 30px;
     display: grid;
-    grid-template-rows: 23px 36px auto;
+    grid-template-rows: 80px auto;
     grid-gap: 20px;
   }
   &__back {
     width: 77px;
     height: 24px;
   }
+  &__block {
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 20px;
+  }
   &__head {
     display: flex;
     justify-content: space-between;
+    flex-direction: column;
   }
   &__title{
     font-weight: 600;
@@ -67,6 +148,8 @@ export default {
     color: #000000;
   }
   &__profile {
+    grid-gap: 20px;
+    display: grid;
     max-height: 280px;
     padding: 20px;
     border-radius: 6px;
@@ -90,13 +173,130 @@ export default {
 }
 
 .profile {
-  &__top {
+  &__left {
     display: grid;
-    grid-template-columns: 151px auto;
+    grid-template-columns: 76px auto;
+    grid-gap: 20px;
+    align-items: center;
+    &_data {
+      grid-template-columns: auto auto;
+    }
+  }
+  &__right {
+    display: grid;
     grid-gap: 20px;
   }
   &__avatar {
     width: 100%;
+  }
+}
+
+.left {
+  &__text {
+    font-weight: 600;
+    font-size: 18px;
+    &_name {
+      font-weight: 500;
+    }
+  }
+  &__date {
+    font-weight: 400;
+    font-size: 14px;
+    color: $black300;
+  }
+}
+
+.right {
+  &__button, &__info, &__bar {
+    border: 1px solid $black0;
+    box-sizing: border-box;
+    border-radius: 6px;
+    &_red {
+      color: $red;
+      border-color: $red100;
+    }
+    &_blue {
+      color: $blue;
+      border-color: $blue100;
+    }
+  }
+  &__button {
+    min-height: 40px;
+  }
+  &__info {
+    display:flex;
+    flex-direction: column;
+    padding: 10px;
+    justify-content: space-around;
+  }
+  &__data-name {
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 130%;
+  }
+  &__data-desc {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 130%;
+    color: $blue;
+  }
+  &__block {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+  }
+  &__bar {
+    padding: 20px 10px;
+  }
+}
+
+.bar {
+  &__info {
+    display: flex;
+    justify-content: space-between;
+  }
+  &__text {
+    font-weight: 600;
+    font-size: 18px;
+    &_small {
+      font-weight: 500;
+      font-size: 16px;
+    }
+    &_grey {
+      font-weight: 500;
+      font-size: 16px;
+      color: $black400;
+    }
+  }
+  &__data {
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 5px;
+  }
+}
+
+@include _1199 {
+  .validator {
+    &__body {
+      padding: 0 20px;
+    }
+    &__block {
+      grid-template-columns: auto;
+      grid-template-rows: repeat(2, 290px);
+    }
+  }
+}
+
+@include _380 {
+  .validator {
+    &__profile {
+      max-height: 100%;
+    }
+  }
+  .bar {
+    &__data {
+      grid-template-columns: auto;
+    }
   }
 }
 </style>
