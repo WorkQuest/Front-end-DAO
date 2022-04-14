c1ccdd417e06f5fd5284ba98a5ac78a5d7e1aa5a
Massi <massi.mez@ya.ru>
2022-04-14 18:24:14 +0700

    feature/26888 add remember me

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,6 +4,8 @@ import { Path } from '~/utils/enums';
 
 // eslint-disable-next-line
 export default function ({ $axios, store, app }, inject) {
+  let isRefreshing = false;
+  let failedQueue = [];
   $axios.onRequest((config) => {
     if (store.getters['user/isAuth'] && !config.url.includes('digitaloceanspaces.com')) {
       const urlName = config.url.split('/').pop();
@@ -19,8 +21,35 @@ export default function ({ $axios, store, app }, inject) {
       await store.dispatch('user/logout');
       await app.$router.push(Path.SIGN_IN);
     } else if (error.response.status === 401) {
-      await store.dispatch('user/refreshTokens');
-      return $axios(originalRequest);
+      const processQueue = (err, token = null) => {
+        failedQueue.forEach((prom) => (err ? prom.reject(err) : prom.resolve(token)));
+        failedQueue = [];
+      };
+      if (isRefreshing) {
+        return new Promise(((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        }))
+          .then((token) => {
+            originalRequest.headers.authorization = `Bearer ${token}`;
+            return $axios(originalRequest);
+          })
+          .catch((err) => Promise.reject(err));
+      }
+      originalRequest._retry = true;
+      isRefreshing = true;
+      return new Promise(((resolve, reject) => {
+        store.dispatch('user/refreshTokens')
+          .then((data) => {
+            originalRequest.headers.authorization = `Bearer ${data.result.access}`;
+            processQueue(null, data.result.access);
+            resolve($axios(originalRequest));
+          })
+          .catch((err) => {
+            processQueue(err, null);
+            reject(err);
+          })
+          .then(() => { isRefreshing = false; });
+      }));
     } else if (error.response.data.code !== 400010) {
       await store.dispatch('main/showToast', {
         title: 'Error',
