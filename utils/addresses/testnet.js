ad43b055e2a98d1ed31b2ee399eeaebc9587ece0
matykoks <matyko.ks@gmail.com>
2022-06-14 15:14:09 +0700

    fixed wq provider for testnet

--- utils/addresses/testnet.js
+++ utils/addresses/testnet.js
@@ -4,7 +4,7 @@ export default {
   // GMAPKEY: process.env.GMAPKEY,
   // SECRET_SUMSUB: process.env.SECRET_SUMSUB,
 
-  WQ_PROVIDER: 'https://testnet-gate.workquest.co/rpc/',
+  WQ_PROVIDER: 'https://testnet-gate.workquest.co/',
   WQ_EXPLORER: 'https://testnet-explorer-api.workquest.co/api/v1',
 
   WORKNET_WBNB_TOKEN: '0xe550018bC9cF68fed303dFB5f225bB0e6B1e201F',
