16c4e1018f03dc07a26e7bbf0ef0202b3c92c8e5
JuSTeRFeeD <justerfeedy@gmail.com>
2022-07-27 13:41:23 +0700

    fix/validators custom input number updated from wq

--- components/ui/BaseInput/index.vue
+++ components/ui/BaseInput/index.vue
@@ -193,30 +193,39 @@ export default {
     focus() {
       if (this.autoFocus) this.$refs.input.focus();
     },
-    enter($event) {
-      this.$emit('enter', $event.target.value);
+    enter(e) {
+      this.$emit('enter', e.target.value);
     },
     input(e) {
       if (this.customType === 'customNumber') {
         let selStart = this.$refs.input.selectionStart;
 
         const { data } = e;
-        if (data) {
-          const index = e?.target?.value?.lastIndexOf('.');
-          const isDot = /[.,]/.test(data);
-          if (/[^0-9.,]/.test(data) || (isDot && index !== -1 && selStart !== e.target.value.length && index < selStart)) {
-            selStart -= 1;
-          }
-        }
+        let val = e.target.value.toString().replace(/,/g, '.').replace(/[^0-9.]/g, '');
+
+        const indexFirst = val.indexOf('.');
+        const indexLast = val.lastIndexOf('.');
+        const isEquals = indexFirst === indexLast;
+        const isDot = /[.,]/.test(data);
+        const isNewDot = isDot && !isEquals && selStart - 1 === indexFirst;
+
+        if (data && data === '.' && val[0] === '.') {
+          selStart += 1;
+        } else if (data && (/[^0-9.,]/.test(data) || (isDot && !isEquals && indexLast !== -1 && selStart !== val.length))) {
+          selStart -= 1;
+        } else if (!data && indexFirst === -1 && indexLast === -1 && selStart === 1 && val[0] === '0') selStart -= 1;
 
         if (e.target.value) {
-          let val = e.target.value.toString().replace(/,/g, '.').replace(/[^0-9.]/g, '');
           const dotIndex = val.indexOf('.');
           if (dotIndex !== -1) {
             const dotIndexLast = val.lastIndexOf('.');
             if (dotIndex !== dotIndexLast) {
               const len = val.length;
-              val = val.substr(0, dotIndex + 1) + val.substr(dotIndex + 1, len).replace(/[.]/g, '');
+              if (!isNewDot) {
+                val = val.substr(0, dotIndex + 1) + val.substr(dotIndex + 1, len).replace(/[.]/g, '');
+              } else {
+                val = val.substr(0, dotIndex + 1).replace(/[.]/g, '') + val.substr(dotIndex + 1, len);
+              }
             }
           }
 
